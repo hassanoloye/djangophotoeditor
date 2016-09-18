f09cffd498edd68843936a9e366f990aa32c8464
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-09-18 12:38:03 +0100

    [Feature #130139871] Make changes to models

--- djangophotoeditor/photos/models.py
+++ djangophotoeditor/photos/models.py
@@ -1,12 +1,17 @@
+from time import time
+
 from django.contrib.auth.models import User
 from django.db import models
+
+
+def get_upload_file_name(instance, filename):
+    return 'photos/user_{0}/{1}'.format(instance.uploader.id, filename)
 
 
 class Base(models.Model):
     """
     Base model inherited by other models
     """
-    name = models.CharField(max_length=100, null=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -14,20 +19,26 @@ class Base(models.Model):
         abstract = True
         ordering = ['-date_modified']
 
+
+class Folder(Base):
+    """Model for folder"""
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        User, related_name='folders', on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
 
-class Folder(Base):
-    """Model for folder"""
-    owner = models.ForeignKey(
-        User, related_name='folders', on_delete=models.CASCADE)
-
-
 class Photo(Base):
     """Model for photo"""
-    image = models.ImageField(upload_to='/uploaded_photos')
-    size = models.IntegerField(default=0)
+    title = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to=get_upload_file_name, null=True)
+    edited_image = models.CharField(max_length=255, null=True)
+    image_size = models.IntegerField(default=0)
     uploader = models.ForeignKey(
         User, related_name='photos', on_delete=models.CASCADE)
     folder = models.ForeignKey(Folder, related_name='photos')
+
+    def __str__(self):
+        return self.title
